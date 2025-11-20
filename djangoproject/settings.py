2f7371fd835d2ca48fd5b5969b9ffea89bc48545
Abdi Gashahun <abdigashahun0@gmail.com>
2025-11-20 17:32:56 +0300

    second

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    'playbackground',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
